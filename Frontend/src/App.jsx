db6e3a774031701e255fdb25a48d934613abe4ee
Muh Roshan <leoparmigiana@gmail.com>
2024-05-06 12:28:26 +0530

    Simple app to Encrypt and Decrypt User Message

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import CryptoJS from 'crypto-js';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+  const [encryptedMessage, setEncryptedMessage] = useState('');
+  const [decryptedMessage, setDecryptedMessage] = useState('');
+
+  const key = import.meta.env.VITE_CRYPTO_KEY; // Replace 'yourSecretKey' with your actual secret key
+
+  const encryptMessage = () => {
+    const iv = CryptoJS.lib.WordArray.random(16); // Generate a random 16-byte IV
+    const encrypted = CryptoJS.AES.encrypt(message, key, { iv: iv }).toString();
+    console.log(encrypted)
+    setEncryptedMessage(encrypted);
+  };
+
+  const decryptMessage = () => {
+    const decrypted = CryptoJS.AES.decrypt(encryptedMessage, key).toString(CryptoJS.enc.Utf8);
+    console.log(decrypted)
+    setDecryptedMessage(decrypted);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col justify-center items-center'>
+      <input
+        type="text"
+        className='input input-bordered w-full max-w-xs'
+        placeholder="Enter your message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button className='m-3 btn bg-gray-500' onClick={encryptMessage}>Encrypt</button>
+      <button className='m-3 btn bg-slate-800' onClick={decryptMessage}>Decrypt</button>
+
+      <div className='m-3'>
+        <h2 >Encrypted Message:</h2>
+        <p>{encryptedMessage}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='m-3'>
+        <h2>Decrypted Message:</h2>
+        <p>{decryptedMessage}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
